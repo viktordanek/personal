511514746ec8a062f4a67dafa8310cac50a6388b
Viktor Danek <viktordanek10@gmail.com>
2025-03-21 09:57:13 -0400

    Closes #3 by introducing a shell with basic development environment

---
+++ shells/shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> { } } :
+    pkgs.mkShell
+        {
+            buildInputs =
+                [
+                    pkgs.coreutils
+                    pkgs.firefox
+                    pkgs.git
+                    pkgs.jetbrains.idea-community
+                    pkgs.jq
+                    pkgs.yq
+                ] ;
+            shellHook =
+                ''
+                    ORIG=$( pwd ) &&
+                    cd $(mktemp -d ) &&
+                        sh ${ builtins.concatStringsSep "" [ "$" "{" "ORIG" "}" ] }/scripts/environment.sh ;
+                '' ;
+        }
