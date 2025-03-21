6bb5e43369056127ef3cdbbaf8676ba84fe8efe1
Viktor Danek <viktordanek10@gmail.com>
2025-03-21 09:40:41 -0400

    yeaH

---
+++ scripts/environment.sh
@@ -0,0 +1,19 @@
+cd $(mktemp -d) &&
+mkdir github &&
+mkdir github/viktordanek &&
+for REPO in personal shell-scripts originator-pid environment-variable string standard-url cache temporary shell-script visitor bash-unit-checker tests invalid-value has-standard-input strip
+do
+  mkdir github/viktordanek/${REPO} &&
+  git -C github/viktordanek/${REPO} init &&
+  git -C github/viktordanek/${REPO} config user.name "Viktor Danek" &&
+  git -C github/viktordanek/${REPO} config user.email "viktordanek10@gmail.com" &&
+  git -C github/viktordanek/${REPO} config core.sshCommand "ssh -i ~/.ssh/victor.danek.id-rsa" &&
+  git -C github/viktordanek/${REPO} remote add origin git@github.com:viktordanek/${REPO}.git &&
+   cat > github/viktordanek/${REPO}/.git/hooks/post-commit <<EOF
+   while ! git push origin HEAD
+   do
+    sleep 1m
+  done
+EOF
+  ) &&
+  chmod 0500 github/viktordanek/${REPO}/.git/hooks/post-commit
