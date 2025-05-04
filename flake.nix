46ddccc0bd33d4a9855c03b6837c95dfecd4bfd3
Viktor Danek <viktordanek10@gmail.com>
2025-05-04 07:52:33 -0400

    use a pinned nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
         {
             environment-variable-lib.url = "github:viktordanek/environment-variable" ;
 	        flake-utils.url = "github:numtide/flake-utils" ;
-	        nixpkgs.url = "github:NixOS/nixpkgs" ;
+	        nixpkgs.url = "github:Nixos/nixpkgs/nixos-22.11" ;
         } ;
     outputs =
         { environment-variable-lib , flake-utils , nixpkgs , self } :
