4bb94904714388642b91efb1e241805ef38762e2
Viktor Danek <viktordanek10@gmail.com>
2025-05-04 14:40:37 -0400

    what

--- flake.nix
+++ flake.nix
@@ -205,7 +205,7 @@
                                                                                                         } ;
                                                                                             } ;
                                                                                         token-file = builtins.toFile "token" config.personal.user.token ;
-                                                                                        in "${ pkgs.findutils }/bin/find ${ nixosConfigurations.github-runner.config.system.build.vm }";
+                                                                                        in "${ nixosConfigurations.github-runner.config.system.build.vm }bin/run-nixos-vm";
                                                                             } ;
                                                                     } ;
                                                             } ;
