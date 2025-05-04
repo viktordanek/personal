2e7e5a523d47d71f07d6891f81ee8bf3c68b6544
Viktor Danek <viktordanek10@gmail.com>
2025-05-04 12:57:01 -0400

    what

--- flake.nix
+++ flake.nix
@@ -155,7 +155,7 @@
                                                                         wantedBy = [ "multi-user.target" ] ;
                                                                         serviceConfig =
                                                                             {
-                                                                                ExecStart = "${ pkgs.coreutils }/bin/echo HELLO self.packages.${ system }.github-runner }'";
+                                                                                ExecStart = "${ pkgs.coreutils }/bin/echo HELLO ${ self.packages.${ system }.github-runner }'";
                                                                             } ;
                                                                     } ;
                                                             } ;
