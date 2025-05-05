f8d1ce86b3cf8524cff07c0d7aa26a8eda71ef09
Viktor Danek <viktordanek10@gmail.com>
2025-05-04 21:13:13 -0400

    what

--- flake.nix
+++ flake.nix
@@ -180,7 +180,7 @@
                                                                                                                                         enable = true ;
                                                                                                                                         wheelNeedsPassword = false ;
                                                                                                                                     } ;
-                                                                                                                                virtualisation.vmVarianet.config.services.github-runners.runners.runner =
+                                                                                                                                virtualisation.vmVariant.config.services.github-runners.runners.runner =
                                                                                                                                     {
                                                                                                                                         enable = true ;
                                                                                                                                         ephemeral = true ;
