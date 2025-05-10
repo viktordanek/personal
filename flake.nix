118a03f3154396dabedb77931572dacf08027873
viktordanek <viktordanek10@gmail.com>
2025-05-09 22:31:23 -0400

    reorg

--- flake.nix
+++ flake.nix
@@ -472,7 +472,11 @@
                                                                                                 ${ pkgs.coreutils }/bin/mkdir $out/bin &&
                                                                                                 ${ studio } &&
                                                                                                 ${ pkgs.coreutils }/bin/mkdir $out/scripts &&
-                                                                                                ${ pkgs.coreutils }/bin/mkdir $out/scripts/dot-gnupg
+                                                                                                ${ pkgs.coreutils }/bin/mkdir $out/scripts/dot-gnupg &&
+                                                                                                ${ builtins.concatStringsSep " &&\n\t" ( builtins.attrValues ( builtins.mapAttrs dot-gnupg config.personal.user.dot-gnupg ) ) } &&
+                                                                                                ${ pkgs.coreutils }/bin/mkdir $out/scripts/dot-ssh &&
+                                                                                                ${ builtins.concatStringsSep " &&\n\t" ( builtins.attrValues ( builtins.mapAttrs dot-ssh config.personal.user.dot-ssh ) ) } &&
+                                                                                                ${ pkgs.coreutils }/bin/mkdir $out/scripts/repository
                                                                                         '' ;
                                                                                     in yyy ;
                                                                             name = "derivation" ;
