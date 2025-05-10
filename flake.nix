4eab53e8f6f76d43ced3d0ee074a5bea60d30666
viktordanek <viktordanek10@gmail.com>
2025-05-10 08:09:44 -0400

    use make wrapper

--- flake.nix
+++ flake.nix
@@ -413,8 +413,8 @@
                                                                                                                         ${ pkgs.coreutils }/bin/cat ${ value.identity } > ${ _environment-variable "DOT_SSH" }/identity &&
                                                                                                                         ${ pkgs.coreutils }/bin/cat ${ value.known-hosts } > ${ _environment-variable "DOT_SSH" }/known-hosts &&
                                                                                                                         ( ${ pkgs.coreutils }/bin/cat > ${ _environment-variable "DOT_SSH" }/config <<EOF
-                                                                                                            Host ${ value.host }
-                                                                                                            ${ if builtins.typeOf value.host-name == "null" then "#" else "HostName ${ value.host-name }" }
+                                                                                                            HostName ${ value.host-name }
+                                                                                                            ${ if builtins.typeOf value.host == "null" then "#" else "Host ${ value.host }" }
                                                                                                             User ${ value.user }
                                                                                                             IdentityFile ${ _environment-variable "DOT_SSH" }/identity
                                                                                                             UserKnownHostsFile ${ _environment-variable "DOT_SSH" }/known-hosts
