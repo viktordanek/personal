330bc8994528509fea6296866c0d09e7c2d918e0
viktordanek <viktordanek10@gmail.com>
2025-05-11 13:27:55 -0400

    added pass scripting

--- flake.nix
+++ flake.nix
@@ -15,23 +15,23 @@
                             lib =
                                 { config , lib , pkgs , ... } :
                                     {
-                                        assertions =
-                                            [
-                                                {
-                                                    assertion = builtins.all ( pass : builtins.hasAttr pass.dot-gnupg config.personal.user.dot-gnupg ) config.personal.user.pass ;
-                                                    message = "All the pass must have defined dot-gnupg" ;
-                                                }
-                                                {
-                                                    assertion = builtins.all ( pass : builtins.hasAttr pass.repository config.personal.user.repository ) config.personal.user.pass ;
-                                                    message = "All the pass must have defined repository" ;
-                                                }
-                                                {
-                                                    assertion = builtins.all ( repository : builtins.hasAttr repository.dot-ssh config.personal.user.dot-ssh ) config.personal.user.repository ;
-                                                    message = "All the repository must have defined repository" ;
-                                                }
-                                            ] ;
                                         config =
                                             {
+                                                assertions =
+                                                    [
+                                                        {
+                                                            assertion = builtins.all ( pass : builtins.hasAttr pass.dot-gnupg config.personal.user.dot-gnupg ) config.personal.user.pass ;
+                                                            message = "All the pass must have defined dot-gnupg" ;
+                                                        }
+                                                        {
+                                                            assertion = builtins.all ( pass : builtins.hasAttr pass.repository config.personal.user.repository ) config.personal.user.pass ;
+                                                            message = "All the pass must have defined repository" ;
+                                                        }
+                                                        {
+                                                            assertion = builtins.all ( repository : builtins.hasAttr repository.dot-ssh config.personal.user.dot-ssh ) config.personal.user.repository ;
+                                                            message = "All the repository must have defined repository" ;
+                                                        }
+                                                    ] ;
                                                 boot.loader =
                                                     {
                                                         efi.canTouchEfiVariables = true ;
