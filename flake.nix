623072c4f1640654baf7a3343f2365a259a7b2ca
viktordanek <viktordanek10@gmail.com>
2025-05-11 22:20:49 -0400

    start the agent

--- flake.nix
+++ flake.nix
@@ -194,16 +194,16 @@
                                                                                                                     fi &&
                                                                                                                     ${ pkgs.coreutils }/bin/mkdir ${ _environment-variable "GNUPGHOME" } &&
                                                                                                                     ${ pkgs.coreutils }/bin/chmod 0700 ${ _environment-variable "GNUPGHOME" } &&
-                                                                                                                    ${ pkgs.gnupg }/bin/gpgconf --homedir ${ _environment-variable "GNUPGHOME" } --create-socketdir &&
-                                                                                                                    ${ pkgs.gnupg }/bin/gpgconf --homedir ${ _environment-variable "GNUPGHOME" } --launch gpg-agent &&
-                                                                                                                    # while [ ! -f ${ _environment-variable "SECRET_KEYS" } ]
-                                                                                                                    # do
-                                                                                                                    #     ${ pkgs.coreutils }/bin/sleep 1
-                                                                                                                    # done &&
-                                                                                                                    # while [ ! -f ${ _environment-variable "OWNERTRUST" } ]
-                                                                                                                    # do
-                                                                                                                    #     ${ pkgs.coreutils }/bin/sleep 1
-                                                                                                                    # done &&
+                                                                                                                    # ${ pkgs.gnupg }/bin/gpgconf --homedir ${ _environment-variable "GNUPGHOME" } --create-socketdir &&
+                                                                                                                    # ${ pkgs.gnupg }/bin/gpgconf --homedir ${ _environment-variable "GNUPGHOME" } --launch gpg-agent &&
+                                                                                                                    while [ ! -f ${ _environment-variable "SECRET_KEYS" } ]
+                                                                                                                    do
+                                                                                                                        ${ pkgs.coreutils }/bin/sleep 1
+                                                                                                                    done &&
+                                                                                                                    while [ ! -f ${ _environment-variable "OWNERTRUST" } ]
+                                                                                                                    do
+                                                                                                                        ${ pkgs.coreutils }/bin/sleep 1
+                                                                                                                    done &&
                                                                                                                     while [ ! -S "$( ${ pkgs.gnupg }/bin/gpgconf --homedir ${ _environment-variable "GNUPGHOME" } --list-dir agent-socket )" ]
                                                                                                                     do
                                                                                                                         ${ pkgs.coreutils }/bin/sleep 1
