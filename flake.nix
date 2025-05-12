e2255505f9bcb934679d0a6c27dfd1ec1db510d1
viktordanek <viktordanek10@gmail.com>
2025-05-12 10:26:11 -0400

    WTF

--- flake.nix
+++ flake.nix
@@ -211,6 +211,7 @@
                                                                                                     propogateBuildInputs =
                                                                                                         [
                                                                                                             pkgs.python3Packages.google-auth
+                                                                                                            pkgs.google-auth-oauthlib
                                                                                                             pkgs.python3Packages.google-api-python-client
                                                                                                         ] ;
                                                                                                 } ;
