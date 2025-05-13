6ee39cce59bf4841cf9e82079ff55fdb5fc63c74
viktordanek <viktordanek10@gmail.com>
2025-05-13 14:13:29 -0400

    added firefox

--- flake.nix
+++ flake.nix
@@ -272,9 +272,9 @@
                                                                                                                 ${ timestamp } &&
                                                                                                                     REPOSITORY=$( ${ _environment-variable "OUT" }/scripts/repository/${ value.repository } ) &&
                                                                                                                     PROFILE=$( ${ pkgs.coreutils }/bin/mktemp --directory ) &&
-                                                                                                                    ${ pkgs.findutils }/bin/find } ${ _environment-variable "REPOSITORY" } -type f -name "*.gpg" | while read GPG_FILE
                                                                                                                     export PASSWORD_STORE_GPG_OPTS="--homedir $( ${ _environment-variable "OUT" }/scripts/dot-gnupg/${ value.dot-gnupg } ) &&
                                                                                                                     export PASSWORD_STORE_DIR=${ _environment-variable "REPOSITORY" } &&
+                                                                                                                    ${ pkgs.findutils }/bin/find ${ _environment-variable "REPOSITORY" } -type f -name "*.gpg" | while read GPG_FILE
                                                                                                                     do
                                                                                                                         REL_PATH=${ _environment-variable "GPG_FILE#${ _environment-variable "REPOSITORY" }" } &&
                                                                                                                             REL_PATH=${ _environment-variable "REL_PATH%.gpg" } &&
