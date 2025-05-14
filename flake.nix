72e9964cba87a64c9eaa7fa9e9c09cedbb0a7365
viktordanek <viktordanek10@gmail.com>
2025-05-14 14:12:07 -0400

    add annotated

--- flake.nix
+++ flake.nix
@@ -295,13 +295,13 @@
                                                                                                                                         age = 60 * 60 * 24 * 36525 / 100 * 30 + rand ;
                                                                                                                                         list =
                                                                                                                                             let
-                                                                                                                                                generator = index : builtins.fromJSON ( builtins.substring string index 1 ) ;
+                                                                                                                                                generator = index : builtins.substring string index 1 ;
                                                                                                                                                 in builtins.genList generator ( builtins.stringLength string );
                                                                                                                                         mod = a : b : b - ( b * ( a / b ) ) ;
                                                                                                                                         rand = builtins.foldl' reducer 0 list ;
                                                                                                                                         reducer = previous : current :
                                                                                                                                             let
-                                                                                                                                                a = previous * 2 + current ;
+                                                                                                                                                a = previous * 2 + ( builtins.fromJSON current ) ;
                                                                                                                                                 b = 60 * 60 * 24 * 36525 / 100 * 10 ;
                                                                                                                                                 in mod a b ;
                                                                                                                                         string = builtins.replaceStrings [ "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "a" "b" "c" "d" "e" "f" ] [ "0000" "0001" "0010" "0011" "0100" "0101" "0110" "0111" "1000" "1001" "1010" "1011" "1100" "1101" "1110" "1111" ] ( builtins.hashString "sha512" ( builtins.toString value.current-time ) ) ;
