88b86eec4b0582c0a69e800d9b3a5f406f3c3709
viktordanek <viktordanek10@gmail.com>
2025-05-15 17:09:26 -0400

    try this

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 	        flake-utils.url = "github:numtide/flake-utils" ;
 	        identity.url = "github:viktordanek/identity/4b069c1d4015986a5e2ff2d8a190cfdd7bef94ec" ;
 	        nixpkgs.url = "github:Nixos/nixpkgs/nixos-24.05" ;
-	        stash-factory.url = "github:viktordanek/stash-factory/e5e5f5ba3fa6d8a90cfec9c66134df352112a617" ;
+	        stash-factory.url = "github:viktordanek/stash-factory/2733c8ce54bd8855d210aa38016739f28fef4477" ;
         } ;
     outputs =
         { environment-variable , flake-utils , identity , nixpkgs , self , stash-factory } :
