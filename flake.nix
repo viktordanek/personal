ec6620d47f4857abe2706d9d7908381e4a471cfd
viktordanek <viktordanek10@gmail.com>
2025-05-16 14:33:57 -0400

    YES

--- flake.nix
+++ flake.nix
@@ -314,6 +314,8 @@
                                                                         ''
                                                                             echo ${ let x = builtins.typeOf primary.identity ; in builtins.trace x x }
                                                                             echo ${ let x = builtins.toJSON ( builtins.attrNames primary.identity ) ; in builtins.trace x x }
+                                                                            echo ${ let x = builtins.typeOf primary.identity.boot ; in builtins.trace x x }
+                                                                            echo ${ let x = builtins.toJSON ( builtins.attrNames primary.identity.boot ) ; in builtins.trace x x }
                                                                         ''
                                                                 )
                                                                 pkgs.trashy
