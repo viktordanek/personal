f7c968f7516e6c193996de25af8622b708d70474
viktordanek <viktordanek10@gmail.com>
2025-05-17 18:33:44 -0400

    WTF

--- flake.nix
+++ flake.nix
@@ -1,15 +1,18 @@
 {
     inputs =
         {
-            cat.url = "github:viktordanek/cat/240f951b90cb669c399c0a82ea9537a4ee8e9559" ;
-	        stash-factory.url = "github:viktordanek/stash-factory/1093e8f9e51bff7024ab0c0f8f74ea3d80d33244" ;
-	        visitor.url = "github:viktordanek/visitor" ;
+            cat.url = "/tmp/tmp.t4PE8uLVvM" ;
+            # cat.url = "github:viktordanek/cat/240f951b90cb669c399c0a82ea9537a4ee8e9559" ;
+            stash-factory.url = "/tmp/tmp.6BOLvK5bZg" ;
+	        # stash-factory.url = "github:viktordanek/stash-factory/1093e8f9e51bff7024ab0c0f8f74ea3d80d33244" ;
+	        visitor.url = "github:viktordanek/visitor/scratch/d926f8ea-1fdc-441c-9fd9-8abbc5e13fdf" ;
         } ;
     outputs =
         { cat , self , stash-factory , visitor } :
             {
                 lib =
                     {
+                        commit-hash ? null ,
                         description ,
                         dot-ssh ? null ,
                         hash-length ? 16 ,
@@ -20,84 +23,116 @@
                         password ,
                         stash ? "stash" ,
                         system ,
-                        time-mask ? "%Y-%m-%d"
+                        time-mask ? "%Y-%m-%d" ,
+                        timestamp ? null
                     } :
                         let
                             primary =
                                 let
                                     cat-lambda =
                                         target : path : value :
-                                            builtins.trace "HI" (
-                                            stash-factory.lib.${ system }.generator ( builtins.trace "130e5a63-19dc-46c0-9032-9bb9b75ca72c" (
-                                                {
-                                                    factory-name = target ;
-                                                    hash-length = primary.hash-length ;
-                                                    generator = cat.lib.generator ;
-                                                    generator-name = target ;
-                                                    generation-parameters =
-                                                        builtins.trace "generation-parameters"
+                                            builtins.toString
+                                                (
+                                                    stash-factory.lib.generator
                                                         {
-                                                            mapping = { "${ target }" = builtins.toString value ; } ;
+                                                            factory-name = target ;
+                                                            hash-length = primary.hash-length ;
+                                                            generator = cat.lib.generator ;
+                                                            generator-name = target ;
+                                                            generation-parameters =
+                                                                {
+                                                                    mapping = { "${ target }" = builtins.toString value ; } ;
+                                                                    nixpkgs = nixpkgs ;
+                                                                    system = system ;
+                                                                } ;
                                                             nixpkgs = nixpkgs ;
-                                                            system = system ;
-                                                        } ;
-                                                    path = builtins.trace "path" [ "cat" target path value ] ;
-                                                    stash-directory = stash-directory ;
-                                                    targets = builtins.trace "target" [ target ] ;
-                                                    time-mask = primary.time-mask ;
-                                                } ) ) ) ;
+                                                            path = [ primary.commit-hash primary.timestamp "cat" target path ] ;
+                                                            stash-directory = stash-directory ;
+                                                            system = primary.system ;
+                                                            targets = [ target ] ;
+                                                        }
+                                                ) ;
                                     in
                                         {
+                                            commit-hash =
+                                                visitor.lib.implementation
+                                                    {
+                                                        null = path : value : "" ;
+                                                        string = path : value : value ;
+                                                    }
+                                                    commit-hash ;
                                             description =
-                                                visitor.lib.${ system }
+                                                visitor.lib.implementation
                                                     {
                                                         string = path : value : value ;
                                                     }
                                                     description ;
                                             hash-length =
-                                                visitor.lib.${ system }
+                                                visitor.lib.implementation
                                                     {
                                                         int = path : value : value ;
                                                     }
                                                     hash-length ;
                                             identity =
-                                                visitor.lib.${ system }
+                                                visitor.lib.implementation
                                                     {
-                                                        path = path : value : builtins.throw "CAUGHT path" ;
-                                                        string = path : value : builtins.throw "CAUGHT string" ;
-                                                        set = path : set : builtins.mapAttrs ( name : value : cat-lambda "identity" path set ) set ;
+                                                        path = path : value : cat-lambda "identity" path value ;
+                                                        string = path : value : cat-lambda "identity" path value ;
                                                     }
                                                     identity ;
                                             known-hosts =
-                                                visitor.lib.${ system }
+                                                visitor.lib.implementation
                                                     {
                                                         path = path : value : cat-lambda "known-hosts" path value ;
                                                         string = path : value : cat-lambda "known-hosts" path value ;
                                                     } ;
                                             name =
-                                                visitor.lib.${ system }
+                                                visitor.lib.implementation
                                                     {
                                                         string = path : value : value ;
                                                     }
                                                     name ;
+                                            nixpkgs =
+                                                visitor.lib.implementation
+                                                    {
+                                                        set = path : set : set ;
+                                                    }
+                                                    nixpkgs ;
                                             password =
-                                                visitor.lib.${ system }
+                                                visitor.lib.implementation
                                                     {
                                                         string = path : value : value ;
                                                     }
                                                     password ;
                                             stash =
-                                                visitor.lib.${ system }
+                                                visitor.lib.implementation
                                                     {
                                                         string = path : value : value ;
                                                     }
                                                     stash ;
+                                            system =
+                                                visitor.lib.implementation
+                                                    {
+                                                        string = path : value : value ;
+                                                    }
+                                                    system ;
                                             time-mask =
-                                                visitor.lib.${ system }
+                                                visitor.lib.implementation
                                                     {
                                                         string = path : value : value ;
                                                     }
                                                     time-mask ;
+                                            timestamp =
+                                                visitor.lib.implementation
+                                                    {
+                                                        bool = path : value : value ;
+                                                        float = path : value : value ;
+                                                        int = path : value : value ;
+                                                        null = path : value : value ;
+                                                        path = path : value : value ;
+                                                        string = path : value : value ;
+                                                    }
+                                                    timestamp ;
                                         } ;
                                     stash-directory = "/home/${ primary.name }/${ primary.stash }" ;
                             in
@@ -322,9 +357,9 @@
                                                                     pkgs.writeShellScriptBin
                                                                         "test-it"
                                                                         ''
-                                                                            echo ${ let x = builtins.toJSON ( builtins.attrNames primary.identity ) ; in builtins.trace "BEFORE" x }
-                                                                            echo ${ let x = builtins.toString primary.identity.boot ; in builtins.trace x x }
-                                                                            echo ${ let x = builtins.toJSON ( builtins.attrNames primary.identity ) ; in builtins.trace "AFTER" x }
+                                                                            echo ${ let x = "" ; in builtins.trace "BEFORE" x }
+                                                                            echo ${ let x = primary.identity.boot ; in builtins.trace x x }
+                                                                            echo ${ let x = "" ; in builtins.trace "AFTER" x }
 
                                                                         ''
                                                                 )
