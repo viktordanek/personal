9f362e32641b98ebbdd73855715f4b147664c46d
Victor Danek <viktordanek10@gmail.com>
2025-05-28 11:18:57 -0400

    fixed post-commit and pre-commit scripts for private

--- flake.nix
+++ flake.nix
@@ -289,8 +289,8 @@
                                                                                                             then
                                                                                                                 (
                                                                                                                     fun() {
-                                                                                                                        GIT_DIR="$1/git" GIT_WORK_TREE="$1/work-tree" git commit -am "" --allow-empty --allow-empty-message
-                                                                                                                        GIT_DIR="$1/git" GIT_WORK_TREE="$1/work-tree" git rev-parse HEAD > "inputs.$2.commit"
+                                                                                                                        env -i HOME="$HOME" PATH="$PATH" GIT_DIR="$1/git" GIT_WORK_TREE="$1/work-tree" git commit -am "" --allow-empty --allow-empty-message < /dev/null
+                                                                                                                        env -i HOME="$HOME" PATH="$PATH"GIT_DIR="$1/git" GIT_WORK_TREE="$1/work-tree" git rev-parse HEAD > "inputs.$2.commit" < /dev/null
                                                                                                                         "git add work-tree/inputs.$2.commit"
                                                                                                                     }
                                                                                                                     fun "$OUT/boot/repository/personal" personal
