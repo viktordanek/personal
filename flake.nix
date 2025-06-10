504ef10177954dac8ea72b8b12c4ed1115d9914d
Victor Danek <viktordanek10@gmail.com>
2025-06-10 10:56:05 -0400

    YES

--- flake.nix
+++ flake.nix
@@ -111,14 +111,15 @@
                                                             dependencies-transitive-closure = builtins.getAttr resource.name dependencies ;
                                                             in
                                                                 {
-                                                                    index =
-                                                                        let
-                                                                            filtered = builtins.filter ( indexed : indexed.value.name == resource.name ) indexed ;
-                                                                            find = builtins.elemAt 0 filtered ;
-                                                                            indexed = builtins.genList ( index : { index = index ; value = builtins.elemAt sorted index ; } ) ( builtins.length sorted ) ;
-                                                                            list = builtins.attrValues ( builtins.mapAttrs ( name : value : { name = name ; value = value ; } ) dependencies ) ;
-                                                                            sorted = builtins.sort ( a : b : if ( builtins.length a.value ) < ( builtins.length b.value ) then true else if ( builtins.length a.value ) > ( builtins.length b.value ) then false else if a.name < b.name then true else if a.name > b.name then false else builtins.throw "identical elements" ) list ;
-                                                                            in find.index ;
+                                                                    index = 0
+#                                                                        let
+#                                                                            filtered = builtins.filter ( indexed : indexed.value.name == resource.name ) indexed ;
+#                                                                            find = builtins.elemAt 0 filtered ;
+#                                                                            indexed = builtins.genList ( index : { index = index ; value = builtins.elemAt sorted index ; } ) ( builtins.length sorted ) ;
+#                                                                            list = builtins.attrValues ( builtins.mapAttrs ( name : value : { name = name ; value = value ; } ) dependencies ) ;
+#                                                                            sorted1 = builtins.sort ( a : b : if ( builtins.length a.value ) < ( builtins.length b.value ) then true else if ( builtins.length a.value ) > ( builtins.length b.value ) then false else if a.name < b.name then true else if a.name > b.name then false else builtins.throw "identical elements" ) list ;
+#                                                                            sorted = builtins.sort ( a : b : true ) list ;
+#                                                                            in find.index ;
                                                                     setup =
                                                                         pkgs.writeShellApplication
                                                                             {
